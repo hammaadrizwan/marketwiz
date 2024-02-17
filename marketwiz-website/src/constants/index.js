397bdb18af25a179fbdc128de760493ef6340210
Hammaad Rizwan <65250595+hammaadrizwan@users.noreply.github.com>
2024-02-17 23:22:32 +0530

    Started Styling index.html based on marketwiz design

--- marketwiz-website/src/constants/index.js
+++ marketwiz-website/src/constants/index.js
@@ -7,15 +7,15 @@ export const navLinks = [
   },
   {
     id: "features",
-    title: "Features",
+    title: "About",
   },
   {
     id: "product",
-    title: "Product",
+    title: "Contact",
   },
   {
-    id: "clients",
-    title: "Clients",
+    id: "login",
+    title: "Login",
   },
 ];
 
@@ -47,7 +47,7 @@ export const feedback = [
   {
     id: "feedback-1",
     content:
-      "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
+      "Money is only a tool but. It will take you wherever you wish, but it will not replace you as the driver.",
     name: "Herman Jensen",
     title: "Founder & Leader",
     img: people01,
