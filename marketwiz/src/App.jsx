01f7583372bae8853176ca6327c06258241d3c4b
Hammaad Rizwan <65250595+hammaadrizwan@users.noreply.github.com>
2024-02-27 23:41:21 +0530

    index.html setup complete

---
+++ marketwiz/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+/* Add the navbar hero team contacts and footer here */
+function App() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
