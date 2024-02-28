c8760b1c0aff0acb37594db03ac604ecc9659e5e
Hammaad Rizwan <65250595+hammaadrizwan@users.noreply.github.com>
2024-02-28 23:15:28 +0530

    Hero section completed

--- marketwiz/src/App.jsx
+++ marketwiz/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 /* Add the navbar hero team contacts and footer here */
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   )
 }
---
+++ marketwiz/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
