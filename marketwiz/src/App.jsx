a993830a7f27750ec7215fde65d6e7fa57405476
Hammaad Rizwan <65250595+hammaadrizwan@users.noreply.github.com>
2024-03-01 00:26:27 +0530

    Completed Info section

--- marketwiz/src/App.jsx
+++ marketwiz/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Info from './components/Info'
 /* Add the navbar hero team contacts and footer here */
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Info />
     </>
   )
 }
---
+++ marketwiz/src/components/Info.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import image from '../assets/right.png'
+
+function Info() {
+    return (
+        <section className="info">
+            <div className="info-items">
+                <div className='info-left'>
+                    <div className='info-left-items'>
+                        <div className='info-left-text'>
+                            <h3>AI powered sales prediction</h3>
+                            <h1>
+                            Revolutionizing the supermarket industry.
+                            </h1>
+                            <h2>
+                            Predict sales using weather with accuracy and enhance stock management now!
+                            </h2>
+                        </div>
+                        <a  href="#" className='info-button font-semibold hover:text-white hover:bg-black'>Get started</a>
+                    </div>
+                </div>
+                <div className='hidden xl:flex info-right'>
+                    <img src={image} alt='' />
+                </div>
+            </div>
+        </section>
+    )
+  }
+  export default Info
