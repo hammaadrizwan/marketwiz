6920ba64f89951657ee1a4d094558052ca918c60
Hammaad Rizwan <65250595+hammaadrizwan@users.noreply.github.com>
2024-03-02 00:09:14 +0530

    Started Contact us now page

--- marketwiz/src/App.jsx
+++ marketwiz/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Info from './components/Info'
 import Team from './components/Team'
+import Contact from './components/Contact'
 /* Add the navbar hero team contacts and footer here */
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
       <Hero />
       <Info />
       <Team />
+      <Contact/>
+      
     </>
   )
 }
---
+++ marketwiz/src/components/Contact.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Contact(){
+    return (
+        <section className="contact">
+            <div className="contact-title">
+                <h1>Contact Us</h1>
+                <p>Lets Grow your sales together</p>
+            </div>
+            <div className="contact-items">
+                <div className="contact-left">
+                    <div className="contact-left-items">
+                        <div className="contact-left-items">
+                    
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+} export default Contact
