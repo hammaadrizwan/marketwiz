00a98322c1facd82422e9558156e608e1460d3cf
Aadhavan Arkhash Saravanakumar <arkhash.0309@gmail.com>
2024-03-02 18:12:35 +0530

    Created new React folder and completed nav bar.

---
+++ dashboard/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import logo from '../assets/logo.svg'
+
+function Navbar() {
+  return (
+    <nav className='nav'>
+      <img className="nav-image" src={logo} alt='marketwiz-logo'></img>
+      <ul className='nav-list '>
+        <li href="#"className='hidden md:flex hover:font-bold'><a href="#" class="">Home</a></li>
+        <li><a href="#"className='hidden md:flex hover:font-bold'>Help</a></li>
+        <a  href="#" className='ml-3 nav-login-button hover:animate-pulse'>Sign up</a>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
