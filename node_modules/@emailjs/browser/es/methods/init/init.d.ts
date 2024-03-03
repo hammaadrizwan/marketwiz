69791d7462bf80bd4991babcf86122fef68dba24
Hammaad Rizwan <65250595+hammaadrizwan@users.noreply.github.com>
2024-03-03 15:16:18 +0530

    completed Contact US form

---
+++ node_modules/@emailjs/browser/es/methods/init/init.d.ts
@@ -0,0 +1,7 @@
+import type { Options } from '../../types/Options';
+/**
+ * Initiation
+ * @param {object} options - the EmailJS global SDK config options
+ * @param {string} origin - the non-default EmailJS origin
+ */
+export declare const init: (options: Options | string, origin?: string) => void;
