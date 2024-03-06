ad5709305e585a7e2615dedc01fd677bdd461339
Aadhavan Arkhash Saravanakumar <arkhash.0309@gmail.com>
2024-03-06 16:55:25 +0530

    renamed the css file for easier merging

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './App.css'
+import './dashboard.css'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Body from './components/Body'
